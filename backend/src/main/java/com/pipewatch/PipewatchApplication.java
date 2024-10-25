1caa64fb46f82bb7c3a23611278d4274b5496305
성철민 <tjdcjfals3@naver.com>
2024-10-25 16:36:36 +0900

    Feat: add .env

--- backend/src/main/java/com/pipewatch/PipewatchApplication.java
+++ backend/src/main/java/com/pipewatch/PipewatchApplication.java
@@ -4,11 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
- * [TODO]
- * DB 설정 후 exclude 설정 제거
- */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class PipewatchApplication {
 
 	public static void main(String[] args) {
