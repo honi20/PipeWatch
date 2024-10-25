9f68959e42466cd6b4f69256986a720d497fb425
성철민 <tjdcjfals3@naver.com>
2024-10-25 17:49:35 +0900

    Feat: add User, Enterprise Entity

---
+++ backend/src/main/java/com/pipewatch/user/model/entity/User.java
@@ -0,0 +1,35 @@
+package com.pipewatch.user.model.entity;
+
+import com.pipewatch.enterprise.model.entity.Enterprise;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Builder
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "user_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "enterprise_id")
+    private Enterprise enterprise;
+
+    @Email
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private State state;
+
+    private Role role;
+}
