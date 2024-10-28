1f2179126bbef5a45cbbf2bae6b481fc5d076cd0
최예헌 <1594cyh@gmail.com>
2024-10-28 15:33:45 +0900

    Fix: BaseEntity 변경
    Spring Data JPA 형식으로 변경

--- backend/src/main/java/com/pipewatch/global/entity/BaseEntity.java
+++ backend/src/main/java/com/pipewatch/global/entity/BaseEntity.java
@@ -1,30 +1,26 @@
 package com.pipewatch.global.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
 @MappedSuperclass
+@SuperBuilder
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
+	@CreatedDate
 	@Column(updatable = false)
 	private LocalDateTime created_at;
 
+	@LastModifiedDate
 	private LocalDateTime updated_at;
 
-	@PrePersist
-	public void persist() {
-		LocalDateTime now = LocalDateTime.now();
-		created_at = now;
-		updated_at = now;
-	}
-
-	@PreUpdate
-	public void preUpdate() {
-		updated_at = LocalDateTime.now();
-	}
 }
