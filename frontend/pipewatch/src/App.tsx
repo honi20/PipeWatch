e82615baba37013e5804297ba9d169ef3dba4a2f
장지현 <hyun051023@gmail.com>
2024-10-28 14:32:30 +0900

    Settings: setup Tailwind

--- frontend/pipewatch/src/App.tsx
+++ frontend/pipewatch/src/App.tsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const handleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <>
-      <h1 className="font-bold underline">Starting Pipe Watch Project</h1>
-    </>
+    <div className="bg-white dark:bg-black w-full h-screen">
+      <h1 className="font-bold underline text-primary-500 dark:text-primary-200">
+        Starting Pipe Watch Project
+      </h1>
+      <button onClick={handleTheme} className="bg-gray-800 dark:bg-gray-500">
+        Switch
+      </button>
+    </div>
   );
 }
 
--- frontend/pipewatch/tailwind.config.js
+++ frontend/pipewatch/tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      primary: {
+        200: "#447FC7",
+        500: "#245795",
+      },
+      white: "#EDEDED",
+      black: "#141414",
+      gray: {
+        200: "#D9D9D9",
+        500: "#9A9A9A",
+        800: "#5E5E5E",
+      },
+      block: "#313F4F",
+      warn: "#FF5353",
+      success: "#499B50",
+      button: {
+        stroke: "#3D444D",
+        background: "#151B23",
+      },
+    },
   },
   plugins: [],
 };
