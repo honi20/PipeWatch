ba13166a3f9dce97185fb4caa57161bdcc382ea7
honi20 <1594cyh@gmail.com>
2024-10-29 00:25:43 +0900

    Feat: AuthDto 설정

---
+++ backend/src/main/java/com/pipewatch/domain/auth/model/dto/AuthDto.java
@@ -0,0 +1,56 @@
+package com.pipewatch.domain.auth.model.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuthDto {
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class EnterpriseRegistRequestDto {
+        private String name;
+        private String industry;
+        private String managerEmail;
+        private String managerPhoneNumber;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class SignupRequestDto {
+        private String email;
+        private String password;
+        private String name;
+        private Long enterpriseId;
+        private Long empNo;
+        private String department;
+        private String empClass;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class EmailCodeSendRequestDto {
+        private String email;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class EmailCodeVerifyRequestDto {
+        private String email;
+        private String code;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class SigninRequestDto {
+        private String email;
+        private String password;
+    }
+}
