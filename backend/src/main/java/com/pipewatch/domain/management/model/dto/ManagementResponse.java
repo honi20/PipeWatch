7129fa039b21ce8ba31857289105ad0b2986c11b
최예헌 <1594cyh@gmail.com>
2024-10-29 11:08:00 +0900

    Feat: ManagerDto 생성

---
+++ backend/src/main/java/com/pipewatch/domain/management/model/dto/ManagementResponse.java
@@ -0,0 +1,60 @@
+package com.pipewatch.domain.management.model.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ManagementResponse {
+	@Getter
+	@Setter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class EmployeeWaitingListDto {
+		List<EmployeeDto> employees;
+
+	}
+
+	@Getter
+	@Setter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class EmployeeListDto {
+		private List<EmployeeDto> employees;
+	}
+
+	@Getter
+	@Setter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class EmployeeSearchDto {
+		private List<EmployeeDto> employees;
+	}
+
+	@Getter
+	@Setter
+	@Builder
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class BuildingListDto {
+		private List<BuildingDto> buildings;
+	}
+
+	private static class EmployeeDto {
+		private String name;
+		private String email;
+		private Long empNo;
+		private String department;
+		private String empClass;
+		private String role;
+	}
+
+	private static class BuildingDto {
+		private String building;
+		private List<Integer> floors;
+	}
+}
