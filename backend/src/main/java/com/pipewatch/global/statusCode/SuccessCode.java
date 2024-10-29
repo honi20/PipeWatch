6a1372318115cc51c1fbf13d9579436504070c52
최예헌 <1594cyh@gmail.com>
2024-10-29 15:43:22 +0900

    Refactor: StatusCode 정리

--- backend/src/main/java/com/pipewatch/global/statusCode/SuccessCode.java
+++ backend/src/main/java/com/pipewatch/global/statusCode/SuccessCode.java
@@ -7,22 +7,31 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessCode {
+	// Auth
 	ENTERPRISE_CREATED(HttpStatus.CREATED.value(), "기업 등록에 성공했습니다."),
 	USER_CREATED(HttpStatus.CREATED.value(), "유저 생성에 성공했습니다."),
 	EMAIL_CODE_SEND_OK(HttpStatus.OK.value(), "이메일 인증 코드를 해당 이메일로 전송했습니다."),
 	EMAIL_CODE_VERIFY_OK(HttpStatus.OK.value(), "이메일 인증에 성공했습니다."),
 	SIGNIN_OK(HttpStatus.OK.value(), "로그인에 성공했습니다."),
 	LOGOUT_OK(HttpStatus.OK.value(), "로그아웃되었습니다."),
+
+	// User
 	MYPAGE_DETAIL_OK(HttpStatus.OK.value(), "개인정보 조회에 성공했습니다."),
 	MYPAGE_MODIFIED_OK(HttpStatus.OK.value(), "개인정보 수정에 성공했습니다."),
 	PASSWORD_MODIFIED_OK(HttpStatus.OK.value(), "비밀번호 수정에 성공했습니다."),
 	USER_DELETE_OK(HttpStatus.NO_CONTENT.value(), "회원 탈퇴에 성공했습니다."),
+
+	// enterprise
 	ENTERPRISE_DETAIL_OK(HttpStatus.OK.value(), "기업 정보 조회에 성공했습니다."),
+
+	// management
 	WAITING_EMPLOYEE_LIST_OK(HttpStatus.OK.value(), "승인 대기 중인 직원 목록 조회에 성공했습니다."),
 	EMPLOYEE_LIST_OK(HttpStatus.OK.value(), "직원 목록 조회에 성공했습니다."),
 	ROLL_MODIFIED_OK(HttpStatus.OK.value(), "권한 변경에 성공했습니다."),
 	EMPLOYEE_SEARCH_OK(HttpStatus.OK.value(), "사원 검색에 성공했습니다."),
 	BUILDING_LIST_OK(HttpStatus.OK.value(), "건물 리스트 조회에 성공했습니다."),
+
+	// pipeline model
 	MODEL_LIST_OK(HttpStatus.OK.value(), "모델 리스트 조회에 성공했습니다."),
 	FILE_UPLOAD_AND_MODEL_CREATED(HttpStatus.CREATED.value(), "GLTF 파일 업로드에 성공했습니다. 파이프라인 모델이 생성되었습니다."),
 	MODEL_INIT_OK(HttpStatus.OK.value(), "모델 초기 정보 설정에 성공했습니다."),
