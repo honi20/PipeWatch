ea49722f09486e391b0ea84fd373f219dae87bc7
장지현 <hyun051023@gmail.com>
2024-10-31 18:04:14 +0900

    feat: Manage Account - 버튼 링크 연결

--- frontend/pipewatch/src/components/manageAccount/dashboard.tsx
+++ frontend/pipewatch/src/components/manageAccount/dashboard.tsx
@@ -1,10 +1,12 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "@headlessui/react";
-// import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
 const Dashboard = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
   return (
     <div className="w-[500px] flex flex-col bg-block rounded-[30px] p-[50px] gap-[40px] text-white">
       <div className="flex justify-center font-semibold text-[28px]">
@@ -14,6 +16,7 @@ const Dashboard = () => {
       <div className="flex flex-col gap-[20px]">
         <Button
           className={`flex items-center h-[56px] w-full px-[30px] text-white rounded-lg bg-button-background`}
+          onClick={() => navigate("/account/manage/edit-info")}
         >
           <ChevronRightIcon />
           {t("manageAccount.dashboard.editInfo")}
@@ -21,6 +24,7 @@ const Dashboard = () => {
 
         <Button
           className={`flex items-center h-[56px] w-full px-[30px] text-white rounded-lg bg-button-background`}
+          onClick={() => navigate("/account/manage/update-pw")}
         >
           <ChevronRightIcon />
           {t("manageAccount.dashboard.updatePassword")}
@@ -28,6 +32,7 @@ const Dashboard = () => {
 
         <Button
           className={`flex items-center h-[56px] w-full px-[30px] text-white rounded-lg bg-button-background`}
+          onClick={() => navigate("/account/manage/withdrawal")}
         >
           <ChevronRightIcon />
           {t("manageAccount.dashboard.withdrawal")}
