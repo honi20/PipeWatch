1e730838e7052b4ebc0e17f762e858c4971a65ae
honi20 <1594cyh@gmail.com>
2024-11-02 23:33:26 +0900

    Test: test db 설정

--- backend/src/test/resources/sql/data.sql
+++ backend/src/test/resources/sql/data.sql
@@ -14,7 +14,7 @@ VALUES (123L, 'test@ssafy.com', '$2a$10$W9WYGcUhi6E2NxqnofecW.DEsMkr42YiOSM8Ou/U
 
 -- enterprise
 INSERT INTO enterprise(enterprise_id, name, industry, manager_email, manager_phone_number, is_active, user_id)
-VALUES (1L, 'paori', '제조업', 'admin@ssafy.com', '010-1234-5678', true, 123L),
+VALUES (1L, 'paori', '제조업', 'admin@ssafy.com', '010-1234-5678', true, 124L),
        (2L, 'samsung', '제조업', 'admin@samsung.com', '010-1234-5678', true, 127L);
 
 -- employeeInfo
@@ -24,7 +24,9 @@ VALUES (123L, 1243242L, 'IT사업부', '팀장', 123L, 1L),
        (125L, 1423435L, '인사부', '부장', 126L, 1L)
        ;
 
-
+-- waiting
+INSERT INTO waiting(waiting_id, user_id, role)
+VALUES (1L, 123L, 'ROLE_EMPLOYEE');
 
 -- building and floor
 INSERT INTO building_and_floor(building_floor_id, enterprise_id, name, floor)
