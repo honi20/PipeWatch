ea6b2d9231d0831635f94c21d13e1d52a4e6920a
honi20 <1594cyh@gmail.com>
2024-11-02 23:53:10 +0900

    Refactor: Role enum 수정

--- backend/src/main/java/com/pipewatch/domain/user/model/entity/Role.java
+++ backend/src/main/java/com/pipewatch/domain/user/model/entity/Role.java
@@ -3,16 +3,10 @@ package com.pipewatch.domain.user.model.entity;
 import java.util.Arrays;
 
 public enum Role {
-	ROLE_USER("일반회원"),
-	ROLE_EMPLOYEE("사원"),
-	ROLE_ADMIN("관리자"),
-	ROLE_ENTERPRISE("기업");
-
-	String value;
-
-	Role(String value) {
-		this.value = value;
-	}
+	ROLE_USER,
+	ROLE_EMPLOYEE,
+	ROLE_ADMIN,
+	ROLE_ENTERPRISE;
 
 	public static boolean isValidRole(String role) {
 		return Arrays.stream(Role.values())
