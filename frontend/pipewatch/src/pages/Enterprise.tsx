f5f071f9c62ff16ec5f610491c01563becd17374
장지현 <hyun051023@gmail.com>
2024-11-12 14:17:07 +0900

    feat: 접근 권한 세팅 1

--- frontend/pipewatch/src/pages/Enterprise.tsx
+++ frontend/pipewatch/src/pages/Enterprise.tsx
@@ -1,18 +1,27 @@
 import { SideBar } from "@src/components/enterprise/SideBar";
 import { Outlet } from "react-router-dom";
 
+import { AccessBlocked } from "@src/components/common/AccessBlocked";
+
 export const Enterprise = () => {
+  const role = localStorage.getItem("role");
+
   return (
     <div className="flex">
-      {/* Sidebar */}
-      <div className="flex flex-col w-[250px] min-h-screen fixed  text-white">
-        <SideBar />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 p-5 ml-[250px]">
-        <Outlet />
-      </div>
+      {role !== "ENTERPRISE" ? (
+        <AccessBlocked />
+      ) : (
+        <>
+          {/* SideBar */}
+          <div className="flex flex-col w-[250px] min-h-screen fixed  text-white">
+            <SideBar />
+          </div>
+          {/* Main content */}
+          <div className="flex-1 p-5 ml-[250px]">
+            <Outlet />
+          </div>
+        </>
+      )}
     </div>
   );
 };
