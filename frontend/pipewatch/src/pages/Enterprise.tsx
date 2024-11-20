f46748ac66a113080b6af9bccca78f3735475631
장지현 <hyun051023@gmail.com>
2024-11-20 09:48:43 +0900

    fix: Enterprise - Role 캐치 전 접근 제한 페이지 뜨는 오류 수정

--- frontend/pipewatch/src/pages/Enterprise.tsx
+++ frontend/pipewatch/src/pages/Enterprise.tsx
@@ -2,27 +2,51 @@ import { SideBar } from "@src/components/enterprise/SideBar";
 import { Outlet } from "react-router-dom";
 
 import { AccessBlocked } from "@src/components/common/AccessBlocked";
-import { useUserStore } from "@src/stores/userStore";
+// import { useUserStore } from "@src/stores/userStore";
+import { useState } from "react";
+import { getApiClient } from "@src/stores/apiClient";
+
+import { useEffect } from "react";
 
 export const Enterprise = () => {
-  const { role } = useUserStore();
+  const [role, setRole] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const saveRole = async () => {
+    const apiClient = getApiClient();
+    try {
+      const res = await apiClient.get("/api/users/profile");
+      const userInfo = res.data.body;
+      setRole(userInfo.role);
+    } catch (err) {
+      console.error("UserInfo 저장 실패", err);
+    } finally {
+      setIsLoading(true);
+    }
+  };
+
+  useEffect(() => {
+    saveRole();
+  }, []);
+
+  if (role === null && isLoading) {
+    return <div>로딩중</div>;
+  }
+
+  if (role !== "ENTERPRISE") {
+    <AccessBlocked />;
+  }
 
   return (
     <div className="flex">
-      {role !== "ENTERPRISE" ? (
-        <AccessBlocked />
-      ) : (
-        <>
-          {/* SideBar */}
-          <div className="flex flex-col w-[250px] min-h-screen fixed  text-white">
-            <SideBar />
-          </div>
-          {/* Main content */}
-          <div className="flex-1 p-5 ml-[250px]">
-            <Outlet />
-          </div>
-        </>
-      )}
+      {/* SideBar */}
+      <div className="flex flex-col w-[250px] min-h-screen fixed  text-white">
+        <SideBar />
+      </div>
+      {/* Main content */}
+      <div className="flex-1 p-5 ml-[250px]">
+        <Outlet />
+      </div>
     </div>
   );
 };
